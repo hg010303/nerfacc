7eacbfc3c1745c4df23e1be4c829209745f7179d
hg010303 <hg010303@naver.com>
2023-11-21 04:33:37 +0000

    first_commit

---
+++ examples/__init__.py
@@ -0,0 +1,13 @@
+from .utils import (
+    MIPNERF360_UNBOUNDED_SCENES,
+    NERF_SYNTHETIC_SCENES,
+    render_image_with_propnet,
+    set_random_seed,
+)
+
+__all__ = (
+    "MIPNERF360_UNBOUNDED_SCENES",
+    "NERF_SYNTHETIC_SCENES",
+    "render_image_with_propnet",
+    "set_random_seed",
+)
